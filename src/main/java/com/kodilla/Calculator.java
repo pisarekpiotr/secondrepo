0a680913c9c2e20897f1df0fa49a00b5a868b820
“pisarekpiotr <you@example.com>
2018-05-30 22:15:45 +0200

    Commit message

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -4,9 +4,19 @@ public class Calculator {
     public void someMethod() {
         System.out.println ("It works!");
     }
+public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
 
     public static void main(String args[]) {
         Calculator calculator = new Calculator();
         calculator.someMethod();
+        System.out.println ("X + Y = " + calculator.add(1,2));
+        System.out.println ("A - B = " + calculator.subtraction(1,2));
     }
 }
