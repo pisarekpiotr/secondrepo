3f1075a2dceba2793a410c04f54901f7882cc9f5
“pisarekpiotr <you@example.com>
2018-06-06 20:53:22 +0200

    Test 2 - Calculator

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/Calculator1.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing;
+
+public class Calculator1 {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int substract(int a, int b) {
+        return a - b;
+   }
+}
+
--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,45 @@
 package com.kodilla.testing;
 
-public class TestingMain {
-    public static void main (String[] args) {
-        System.out.println ("Moduł 6. Wprowadzenie do testowania oprogramowania");
+import com.kodilla.testing.SimpleUser;
+import com.kodilla.testing.Calculator1;
 
+
+public class TestingMain {
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        //Poniżej mój kolejny test -->
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator1 firstCalculator = new Calculator1();
+        int result2 = firstCalculator.add (1,2);
+        int result3 = firstCalculator.substract(1,2);
+
+        System.out.println();
+        System.out.println("A + B = 4 ");
+        if (result2 == 4 ) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("A + B = "  + result2);
+
+        System.out.println();
+        System.out.println("A - B = 4 ");
+        if (result3 == 4 ) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("A - B = "  + result3);
+      }
     }
-}
+
