e32ddd6deb49c2e57ae38e0b60e0ef1f361465f6
“pisarekpiotr <you@example.com>
2018-07-25 20:13:12 +0200

    7.3 Update(LocalDate.now())

--- kodilla-stream/src/main/java/com/kodilla/stream/main.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/main.java
@@ -16,7 +16,8 @@ public class main {
         Forum theForum = new Forum();
         Map <String,ForumUser > theForumResults = theForum.getList().stream()
                 .filter(ForumUser -> ForumUser.getSex() == 'M')
-                .filter(ForumUser -> ForumUser.getLocalDate().getYear() < LocalDate.of(2018,7,24).getYear() - 20)
+                //.filter(ForumUser -> ForumUser.getLocalDate().getYear() < LocalDate.of(2018,7,24).getYear() - 20)
+                .filter(ForumUser -> ForumUser.getLocalDate().getYear() < LocalDate.now().getYear() - 20)
                 .filter(ForumUser -> ForumUser.getPosts() > 1)
                 .collect(Collectors.toMap(ForumUser::getUserSignature,Forum -> Forum));
 
